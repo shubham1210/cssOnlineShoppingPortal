251293a6a4279835405603d7fd5befaf0fc742fb
SHUBHAM SHARMA <shubham1210@icloud.com>
2020-06-28 18:54:36 +0530

    addding responsive changes for mobile

--- bundle.js
+++ bundle.js
@@ -8,3 +8,17 @@ $(document).ready(function () {
     }
   );
 });
+
+function showButton(from) {
+  if (from == "bar") {
+    $("#barsIcon").css("display", "none");
+    $("#closeIcon").css("display", "block");
+    $(".content-all").css("display", "none");
+    $(".menubar-mobile").css("display", "flex");
+  } else {
+    $("#barsIcon").css("display", "block");
+    $("#closeIcon").css("display", "none");
+    $(".content-all").css("display", "block");
+    $(".menubar-mobile").css("display", "none");
+  }
+}
